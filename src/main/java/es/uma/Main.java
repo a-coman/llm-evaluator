d54cb7697405e767adfbd26e95d9c7a0869c3a55
a-coman <141495654+a-coman@users.noreply.github.com>
2025-06-23 17:24:58 +0200

    Move path processing to Utils

--- src/main/java/es/uma/Main.java
+++ src/main/java/es/uma/Main.java
@@ -1,13 +1,9 @@
 package es.uma;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Main {
 
@@ -21,89 +17,6 @@ public class Main {
             *(If there is only one attribute, it also returns 1.0)*
             - **0.0** means the attributes are entirely **identical**.
             """;
-
-    private Map<String, Map<String, List<String>>> getPaths(String type) {
-        Map<String, Map<String, List<String>>> simplePaths = new HashMap<>();
-        Map<String, Map<String, List<String>>> cotPaths = new HashMap<>();
-        String datasetRoot = "./src/main/resources/dataset";
-        File rootDir = new File(datasetRoot);
-        for (File categoryDir : rootDir.listFiles()) {
-            if (categoryDir.isDirectory()) {
-                if (categoryDir.getName().equals("Simple")) {
-                    processSimpleDirectory(categoryDir, simplePaths);
-                } else if (categoryDir.getName().equals("CoT")) {
-                    processCotDirectory(categoryDir, cotPaths);
-                }
-            }
-        }
-
-        switch (type) {
-            case "Simple":
-                return simplePaths;
-            case "CoT":
-                return cotPaths;
-            default:
-                throw new RuntimeException("Invalid type: " + type);
-        }
-    }
-
-    private void processSimpleDirectory(File simpleDir, Map<String, Map<String, List<String>>> simplePaths) {
-        for (File systemDir : simpleDir.listFiles()) {
-            if (systemDir.isDirectory()) {
-                for (File timestampDir : systemDir.listFiles()) {
-                    if (timestampDir.isDirectory()) {
-                        String systemKey = systemDir.getName();
-                        Map<String, List<String>> genMap = new TreeMap<>(new GenComparator());
-                        for (File genDir : timestampDir.listFiles()) {
-                            if (genDir.isDirectory() && genDir.getName().startsWith("gen")) {
-                                File outputFile = new File(genDir, "output.soil");
-                                if (outputFile.exists()) {
-                                    genMap.put(genDir.getName(), Collections.singletonList(outputFile.getAbsolutePath()));
-                                }
-                            }
-                        }
-                        simplePaths.put(systemKey, genMap);
-                    }
-                }
-            }
-        }
-    }
-
-    private void processCotDirectory(File cotDir, Map<String, Map<String, List<String>>> cotPaths) {
-        for (File systemDir : cotDir.listFiles()) {
-            if (systemDir.isDirectory()) {
-                for (File timestampDir : systemDir.listFiles()) {
-                    if (timestampDir.isDirectory()) {
-                        String systemKey = systemDir.getName();
-                        Map<String, List<String>> genMap = new TreeMap<>(new GenComparator());
-                        for (File genDir : timestampDir.listFiles()) {
-                            if (genDir.isDirectory() && genDir.getName().startsWith("gen")) {
-                                List<String> categoryFiles = new ArrayList<>();
-                                for (File file : genDir.listFiles()) {
-                                    if (file.getName().endsWith(".soil") && !file.getName().contains("output") && !file.getName().contains("temp")) {
-                                        categoryFiles.add(file.getAbsolutePath());
-                                    }
-                                }
-                                genMap.put(genDir.getName(), categoryFiles);
-                            }
-                        }
-                        cotPaths.put(systemKey, genMap);
-                    }
-                }
-            }
-        }
-    }
-
-    // Custom Comparator for numerical ordering of "genX" strings
-    private static class GenComparator implements Comparator<String> {
-        @Override
-        public int compare(String gen1, String gen2) {
-            // Extract numeric part after "gen"
-            int num1 = Integer.parseInt(gen1.replace("gen", ""));
-            int num2 = Integer.parseInt(gen2.replace("gen", ""));
-            return Integer.compare(num1, num2);
-        }
-    }
 
     private static String calculateSimpleSimilarities(Map<String, Map<String, List<String>>> simplePaths) {
         StringBuilder output = new StringBuilder();
@@ -259,8 +172,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Map<String, Map<String, List<String>>> simplePaths = new Main().getPaths("Simple");
-        Map<String, Map<String, List<String>>> cotPaths = new Main().getPaths("CoT");
+        Map<String, Map<String, List<String>>> simplePaths = Utils.getPaths("Simple");
+        Map<String, Map<String, List<String>>> cotPaths = Utils.getPaths("CoT");
         System.out.println("SimplePaths:\n" + simplePaths + "\n\n");
         System.out.println("CoTPaths:\n" + cotPaths + "\n\n");
         
--- src/main/java/es/uma/Utils.java
+++ src/main/java/es/uma/Utils.java
@@ -5,7 +5,12 @@ import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -64,12 +69,101 @@ public class Utils {
         return match(instance, pattern);
     }
 
+    // Custom Comparator for numerical ordering of "genX" strings
+    private static class GenComparator implements Comparator<String> {
+        @Override
+        public int compare(String gen1, String gen2) {
+            // Extract numeric part after "gen"
+            int num1 = Integer.parseInt(gen1.replace("gen", ""));
+            int num2 = Integer.parseInt(gen2.replace("gen", ""));
+            return Integer.compare(num1, num2);
+        }
+    }
+
+    public static Map<String, Map<String, List<String>>> getPaths(String type) {
+        Map<String, Map<String, List<String>>> simplePaths = new HashMap<>();
+        Map<String, Map<String, List<String>>> cotPaths = new HashMap<>();
+        String datasetRoot = "./src/main/resources/dataset";
+        File rootDir = new File(datasetRoot);
+        for (File categoryDir : rootDir.listFiles()) {
+            if (categoryDir.isDirectory()) {
+                if (categoryDir.getName().equals("Simple")) {
+                    processSimpleDirectory(categoryDir, simplePaths);
+                } else if (categoryDir.getName().equals("CoT")) {
+                    processCotDirectory(categoryDir, cotPaths);
+                }
+            }
+        }
+
+        switch (type) {
+            case "Simple":
+                return simplePaths;
+            case "CoT":
+                return cotPaths;
+            default:
+                throw new RuntimeException("Invalid type: " + type);
+        }
+    }
+
+    private static void processSimpleDirectory(File simpleDir, Map<String, Map<String, List<String>>> simplePaths) {
+        for (File systemDir : simpleDir.listFiles()) {
+            if (systemDir.isDirectory()) {
+                for (File timestampDir : systemDir.listFiles()) {
+                    if (timestampDir.isDirectory()) {
+                        String systemKey = systemDir.getName();
+                        Map<String, List<String>> genMap = new TreeMap<>(new GenComparator());
+                        for (File genDir : timestampDir.listFiles()) {
+                            if (genDir.isDirectory() && genDir.getName().startsWith("gen")) {
+                                File outputFile = new File(genDir, "output.soil");
+                                if (outputFile.exists()) {
+                                    genMap.put(genDir.getName(), Collections.singletonList(outputFile.getAbsolutePath()));
+                                }
+                            }
+                        }
+                        simplePaths.put(systemKey, genMap);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void processCotDirectory(File cotDir, Map<String, Map<String, List<String>>> cotPaths) {
+        for (File systemDir : cotDir.listFiles()) {
+            if (systemDir.isDirectory()) {
+                for (File timestampDir : systemDir.listFiles()) {
+                    if (timestampDir.isDirectory()) {
+                        String systemKey = systemDir.getName();
+                        Map<String, List<String>> genMap = new TreeMap<>(new GenComparator());
+                        for (File genDir : timestampDir.listFiles()) {
+                            if (genDir.isDirectory() && genDir.getName().startsWith("gen")) {
+                                List<String> categoryFiles = new ArrayList<>();
+                                for (File file : genDir.listFiles()) {
+                                    if (file.getName().endsWith(".soil") && !file.getName().contains("output") && !file.getName().contains("temp")) {
+                                        categoryFiles.add(file.getAbsolutePath());
+                                    }
+                                }
+                                genMap.put(genDir.getName(), categoryFiles);
+                            }
+                        }
+                        cotPaths.put(systemKey, genMap);
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String path = "src/main/resources/dataset/CoT/Football/02-04-2025--19-03-28/gen5/invalid.soil";
-        String instance = readFile(path);
-        List<Double> numericAttributes = getNumericAttributes(instance);
-        List<String> stringAttributes = getStringAttributes(instance);
-        System.out.println(numericAttributes);
-        System.out.println(stringAttributes);
+        // String path = "src/main/resources/dataset/CoT/Football/02-04-2025--19-03-28/gen5/invalid.soil";
+        // String instance = readFile(path);
+        // List<Double> numericAttributes = getNumericAttributes(instance);
+        // List<String> stringAttributes = getStringAttributes(instance);
+        // System.out.println(numericAttributes);
+        // System.out.println(stringAttributes);
+
+        // Map<String, Map<String, List<String>>> simplePaths = Utils.getPaths("Simple");
+        Map<String, Map<String, List<String>>> cotPaths = Utils.getPaths("CoT");
+        // System.out.println("SimplePaths:\n" + simplePaths + "\n\n");
+        System.out.println(cotPaths.get("Bank").get("gen1"));
+        // System.out.println("CoTPaths:\n" + cotPaths + "\n\n");
     }
 }
