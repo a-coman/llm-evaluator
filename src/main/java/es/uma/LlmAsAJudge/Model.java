48668a652c52ff6536a073d1373be9e0a031846f
a-coman <141495654+a-coman@users.noreply.github.com>
2025-12-09 22:00:58 +0100

    Update models to OpenRouter

--- src/main/java/es/uma/LlmAsAJudge/Model.java
+++ src/main/java/es/uma/LlmAsAJudge/Model.java
@@ -1,9 +1,10 @@
 package es.uma.LlmAsAJudge;
 
 public enum Model {
-    GEMINI_2_5_FLASH(""),
+    GEMINI_2_5_FLASH("google/gemini-2.5-flash"),
     GPT_OSS(""),
     CLAUDE_OPUS_4_5(""),
+    GEMINI_3_PRO("google/gemini-3-pro-preview"),
     TEST("gemini-2.5-flash");
     
     private final String modelName;
